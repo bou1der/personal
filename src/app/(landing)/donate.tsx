97884eeac6031dcf9e1ec1e5fa752f4c8b9a19e3
bou1der <krowkin@bk.ru>
2025-01-22 21:01:58 +0300

    Update

--- src/app/(landing)/donate.tsx
+++ src/app/(landing)/donate.tsx
@@ -6,7 +6,7 @@ import { Button } from "~/components/ui/button";
 export function DonateSection(){
 
   return (
-    <Button disabled className="cursor-not-allowed absolute group top-4 right-4 h-16 bg-white/5 text-white hover:text-black transition-colors rounded-2xl backdrop-blur-sm font-semibold text-lg border border-input">
+    <Button disabled className="cursor-not-allowed absolute group top-4  right-4 h-14 bg-white/5 text-white hover:text-black transition-colors rounded-2xl backdrop-blur-sm font-semibold text-lg border border-input">
       <DonateIcon className="!size-8 transition-all duration-300 invert group-hover:invert-0" />
       Пожертвовать
     </Button>
